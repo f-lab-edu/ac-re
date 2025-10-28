6032330bee76acc406a785ef9df55a91f3087af0
qxent300 <qxent300@naver.com>
2025-10-28 20:10:31 +0900

    feat: 아이디, 비밀번호를 입력하여 로그인하는 로직 추가
    아이디, 비밀번호를 입력받아 DB에서 조회하여 일치하는 데이터가 있는 경우 해당 회원 정보를, 데이터가 없는 경우 500 에러를 리턴.

--- src/main/java/com/project/acre/controller/MemberController.java
+++ src/main/java/com/project/acre/controller/MemberController.java
@@ -1,8 +1,8 @@
 package com.project.acre.controller;
 
-import com.project.acre.domain.Member;
 import com.project.acre.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -20,11 +20,6 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @GetMapping("/members/login")
-    public String login() {
-        return "members/login";
-    }
-
     @GetMapping("/members/join")
     public ModelAndView joinMember() {
         return new ModelAndView("members/joinMember");
@@ -38,6 +33,22 @@ public class MemberController {
 
     @ExceptionHandler(value = NoSuchAlgorithmException.class)
     public ResponseEntity<String> handleException(NoSuchAlgorithmException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+
+    @GetMapping("/members/login")
+    public ModelAndView login() {
+        return new ModelAndView("members/login");
+    }
+
+    @PostMapping("/members/login")
+    @ResponseBody
+    public ResponseEntity<MemberDto> login(@RequestBody MemberDto memberDto) throws Exception {
+        return new ResponseEntity<>(memberService.login(memberDto.getId(), memberDto.getPassword()), HttpStatus.OK);
+    }
+
+    @ExceptionHandler(value = EmptyResultDataAccessException.class)
+    public ResponseEntity<String> handleException(EmptyResultDataAccessException e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 }
--- src/main/java/com/project/acre/service/MemberService.java
+++ src/main/java/com/project/acre/service/MemberService.java
@@ -5,11 +5,13 @@ import com.project.acre.domain.Member;
 import com.project.acre.repository.MemberRepository;
 import com.project.acre.util.SHA256;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
+import java.util.Optional;
 
 @Service
 public class MemberService {
@@ -20,11 +22,6 @@ public class MemberService {
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
-
-    public Member login(String id,String password) {
-        return null;
-    }
-
 
     public MemberDto join(MemberDto memberDto) throws NoSuchAlgorithmException {
         Member member = new Member();
@@ -42,5 +39,25 @@ public class MemberService {
         memberDto.setKey(member.getKey());
 
         return memberDto;
+    }
+
+    public MemberDto login(String id, String password) throws NoSuchAlgorithmException {
+        Optional<Member> result = memberRepository.findMember(id, sha256.encrypt(password));
+
+        if (result.isEmpty()) {
+            throw new EmptyResultDataAccessException(1);
+        } else {
+            MemberDto memberDto = new MemberDto();
+            memberDto.setId(result.get().getId());
+            memberDto.setPassword(result.get().getPassword());
+            memberDto.setName(result.get().getName());
+            memberDto.setNickname(result.get().getNickname());
+            memberDto.setBirth(result.get().getBirth());
+            memberDto.setEmail(result.get().getEmail());
+            memberDto.setPhone(result.get().getPhone());
+            memberDto.setClassify(result.get().getClassify());
+
+            return memberDto;
+        }
     }
 }
